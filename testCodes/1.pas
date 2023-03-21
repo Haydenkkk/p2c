b53500312b586fb86751aa482938cb38d8a98c92
haydenkkk <lihaoxin67@gmail.com>
2023-03-21 23:57:49 +0800

    update

--- testCodes/1.pas
+++ testCodes/1.pas
@@ -1,10 +1,11 @@
-Program example;
-
-Var c, d : integer;
-Function gcd( a, b : integer) : integer;
-Begin
-  If b = 0 Then gcd := a
-End;
-Begin
-  write(gcd(c, d))
-End.
+{program example(input,output);
+    var x,y:integer;
+    function gcd(a,b:integer):integer;{
+        begin 
+            if b=0 then gcd:=a}
+            else gcd:=gcd(b, a mod b)
+        end;}
+    begin
+        read(x, y);
+        write(gcd(x, y))
+    end.
