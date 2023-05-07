b3c33f456c254484bb7a672dd069105fd12a7fc6
haydenkkk <lihaoxin67@gmail.com>
2023-05-07 21:40:59 +0800

    update flask server

--- scanner.py
+++ scanner.py
@@ -210,15 +210,15 @@ class Scanner(object):
         t.lexer.skip(1)
 
 
-scanner = Scanner()
-scanner.build()
-fin = "testcodes/1.pas"
-f = open(fin, "r")
-data = f.read()
-scanner.input(data)
-while True:
-    tok = scanner.token()
-    if not tok:
-        break
-    print(tok)
+# scanner = Scanner()
+# scanner.build()
+# fin = "testcodes/1.pas"
+# f = open(fin, "r")
+# data = f.read()
+# scanner.input(data)
+# while True:
+#     tok = scanner.token()
+#     if not tok:
+#         break
+#     print(tok)
 # print(scanner.comment)
